0d04d41b11826be7de7ae51ee4c63d44f7d726cb
Iain MacDonald <IJMacD@gmail.com>
2019-07-12 17:40:08 +0800

    Super simple expressions

--- src/interpreter.js
+++ src/interpreter.js
@@ -2,57 +2,154 @@
  *
  * @param {string} command
  * @param {{ [name: string]: any }} context
- * @param {({ [name: string]: any }) => void} setContext
+ * @param {(context: { [name: string]: any }) => void} setContext
  */
 export default function interpreter (command, context, setContext) {
-    const stringMatch = /^"([^"]*)"$/.exec(command)
-    if (stringMatch) {
-        return JSON.stringify(stringMatch[1]);
-    }
-
-    const assignLeftStringMatch = /^([a-z][a-z0-9_]*) <- "([^"]*)"$/.exec(command)
-    if (assignLeftStringMatch) {
-        setContext({
-            ...context,
-            [assignLeftStringMatch[1]]: assignLeftStringMatch[2],
-        });
-        return;
-    }
-
-    const assignRightStringMatch = /^"([^"]*)" -> ([a-z][a-z0-9_]*)$/.exec(command)
-    if (assignRightStringMatch) {
-        setContext({
-            ...context,
-            [assignRightStringMatch[2]]: assignRightStringMatch[1],
-        });
-        return;
-    }
-
-    const assignLeftNumberMatch = /^([a-z][a-z0-9_]*) <- ([0-9]+(?:\.[0-9]+)?)$/.exec(command)
-    if (assignLeftNumberMatch) {
-        setContext({
-            ...context,
-            [assignLeftNumberMatch[1]]: +assignLeftNumberMatch[2],
-        });
-        return;
-    }
-
-    const assignRightNumberMatch = /^([0-9]+(?:\.[0-9]+)?) -> ([a-z][a-z0-9_]*)$/.exec(command)
-    if (assignRightNumberMatch) {
-        setContext({
-            ...context,
-            [assignRightNumberMatch[2]]: +assignRightNumberMatch[1],
-        });
-        return;
-    }
-
-    if (command in context) {
-        return JSON.stringify(context[command]);
-    }
-
     if (command.length === 0) {
         return;
     }
 
+    const tokens = tokenizer(command);
+
+    if (tokens.length === 1) {
+        if (tokens[0].type === "name") {
+            if (tokens[0].value in context) {
+                return JSON.stringify(context[tokens[0].value]);
+            } else {
+                throw Error("symbol not found: " + tokens[0].value);
+            }
+        }
+
+        if (tokens[0].type === "number" || tokens[0].type === "string") {
+            return JSON.stringify(tokens[0].value);
+        }
+
+        throw Error("Invalid expression");
+    }
+
+    if (tokens.length === 3) {
+        const t1 = tokens[0];
+        const t2 = tokens[1];
+        const t3 = tokens[2];
+
+        if (t2.type !== "operator")
+        {
+            throw Error("Expression not supported");
+        }
+
+        switch (t2.value) {
+            case "<-":
+                if (t1.type !== "name" || (t3.type !== "string" && t3.type !== "number")) {
+                    throw Error(`Invalid operands to '<-': [${t1.value}, ${t3.value}]`);
+                }
+                setContext({
+                    ...context,
+                    [t1.value]: t3.value,
+                });
+                return;
+            case "->":
+                if (t3.type !== "name" || (t1.type !== "string" && t1.type !== "number")) {
+                    throw Error(`Invalid operands to '->': [${t1.value}, ${t3.value}]`);
+                }
+                setContext({
+                    ...context,
+                    [t3.value]: t1.value,
+                });
+                return;
+            case "+": {
+                const v1 = checkNumericValue(context, t1);
+                const v3 = checkNumericValue(context, t3);
+                return v1 + v3;
+            }
+            case "-": {
+                const v1 = checkNumericValue(context, t1);
+                const v3 = checkNumericValue(context, t3);
+                return v1 - v3;
+            }
+            case "*": {
+                const v1 = checkNumericValue(context, t1);
+                const v3 = checkNumericValue(context, t3);
+                return v1 * v3;
+            }
+            case "/": {
+                const v1 = checkNumericValue(context, t1);
+                const v3 = checkNumericValue(context, t3);
+                return v1 / v3;
+            }
+        }
+    }
+
     throw Error(`Command not recognised: '${command}'`);
+}
+
+const GRAMMAR = {
+    string: {
+        match: /^"([^"]*)"/,
+        map: m => m[1],
+    },
+    number: {
+        match: /^-?[0-9]+(?:\.[0-9]+)?/,
+        map: m => +m[0],
+    },
+    name: {
+        match: /^[a-z][a-z0-9_]*/,
+    },
+    operator: {
+        match: /^(<-|->|\+|-|\*|\/)/,
+    },
+};
+
+/**
+ *
+ * @param {string} input
+ */
+function tokenizer (input) {
+    let i = 0;
+    const tokens = [];
+
+    while (i < input.length) {
+        let tail = input.substr(i);
+
+        for (const type in GRAMMAR) {
+            const g = GRAMMAR[type];
+            const match = g.match.exec(tail);
+            if (match) {
+                tokens.push({
+                    type,
+                    value: g.map ? g.map(match) : match[0],
+                });
+                i += match[0].length;
+                break;
+            }
+        }
+
+        if (i < input.length) {
+            tail = input.substr(i);
+
+            const whitespaceMatch = /^\s+/.exec(tail);
+            if (whitespaceMatch) {
+                i += whitespaceMatch[0].length;
+                continue;
+            }
+
+            throw Error("Unrecognised Input: " + tail.substr(0, 10));
+        }
+    }
+
+    return tokens;
+}
+
+function checkNumericValue (context, token) {
+
+    if (token.type !== "number" && token.type !== "name") {
+        throw Error(`Invalid numeric value: [${token.value}]`);
+    }
+
+    const v = token.type === "name" ? context[token.value] : token.value;
+
+    if (typeof v === "undefined") {
+        throw Error("symbol not found: " + token.value);
+    }
+
+    return v;
 }
